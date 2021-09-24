22ad21e299277b36982b43b4cba36820209462ad
Amanda Lucas <amandalucaspereira@gmail.com>
2021-09-24 19:39:10 -0300

    save test tiles when running preprocessing

--- pre_processing.py
+++ pre_processing.py
@@ -45,26 +45,43 @@ print('Total deforestaion class is {}'.format(len(final_mask[final_mask==1])))
 print('Total past deforestaion class is {}'.format(len(final_mask[final_mask==2])))
 print('Percentage of deforestaion class is {:.2f}'.format((len(final_mask[final_mask==1])*100)/len(final_mask[final_mask==0])))
 
-################### EXTRACT PATCHES
 
-print("[*] EXTRACTING PATCHES")
+################### EXTRACT TILES
 
 trn_out_path = config['output_path'] + '/training_data'
 val_out_path = config['output_path'] + '/validation_data'
 tst_out_path = config['output_path'] + '/testing_data'
-rej_out_path = config['output_path'] + '/trained_input'
-
-os.makedirs(trn_out_path + '/imgs', exist_ok=True)
-os.makedirs(trn_out_path + '/masks', exist_ok=True)
-os.makedirs(val_out_path + '/imgs', exist_ok=True)
-os.makedirs(val_out_path + '/masks', exist_ok=True)
-os.makedirs(tst_out_path + '/imgs', exist_ok=True)
-os.makedirs(tst_out_path + '/masks', exist_ok=True)
 
 # Create tile mask
 mask_tiles = create_mask(final_mask.shape[0], final_mask.shape[1], grid_size=(5, 4))
 image_array = image_array[:mask_tiles.shape[0], :mask_tiles.shape[1],:]
 final_mask = final_mask[:mask_tiles.shape[0], :mask_tiles.shape[1]]
+
+if config['save_tiles']:
+    os.makedirs(tst_out_path + '/tiles_ts', exist_ok=True)
+    for num_tile in tiles_ts:
+        rows, cols = np.where(mask_tiles == num_tile)
+        x1 = np.min(rows)
+        y1 = np.min(cols)
+        x2 = np.max(rows)
+        y2 = np.max(cols)
+        tile_img = image_array[x1:x2 + 1, y1:y2 + 1, :]
+        tile_ref = final_mask[x1:x2 + 1, y1:y2 + 1]
+        np.save(tst_out_path + '/tiles_ts/' + str(num_tile) + '_img.npy', tile_ref)
+        np.save(tst_out_path + '/tiles_ts/' + str(num_tile) + '_ref.npy', tile_ref)
+        if config['change_detection']:
+            h, w, c = tile_img.shape
+            cv2.imwrite(tst_out_path + '/tiles_ts/' + str(num_tile) + '_t1.jpeg', tile_img[:,:,:c//2])
+            cv2.imwrite(tst_out_path + '/tiles_ts/' + str(num_tile) + '_t2.jpeg', tile_img[:,:,c//2:])
+        else:
+            cv2.imwrite(tst_out_path + '/tiles_ts/' + str(num_tile) + '_img.jpeg', tile_img)
+
+exit()
+
+################### EXTRACT PATCHES
+
+
+print("[*] EXTRACTING PATCHES")
 
 patches_trn, patches_trn_ref = patch_tiles(config['tiles_tr'], mask_tiles, image_array, final_mask, config['patch_size'], stride)
 patches_val, patches_val_ref = patch_tiles(config['tiles_val'], mask_tiles, image_array, final_mask, config['patch_size'], stride)
@@ -79,6 +96,12 @@ print('[*] Validation patches:', patches_val.shape)
 print('[*] Testing patches:', patches_tst.shape)
 
 if config['save_patches']:
+    os.makedirs(trn_out_path + '/imgs', exist_ok=True)
+    os.makedirs(trn_out_path + '/masks', exist_ok=True)
+    os.makedirs(val_out_path + '/imgs', exist_ok=True)
+    os.makedirs(val_out_path + '/masks', exist_ok=True)
+    os.makedirs(tst_out_path + '/imgs', exist_ok=True)
+    os.makedirs(tst_out_path + '/masks', exist_ok=True)
     print("[*] SAVING PATCHES")
     print('Saving training patches...')
     write_patches_to_disk(patches_trn, patches_trn_ref, trn_out_path)
@@ -102,6 +125,7 @@ del patches_trn, patches_trn_ref, patches_val, patches_val_ref, patches_tst, pat
 
 if config['create_input_pix2pix']:
     print('[*] CREATING INPUT FOR THE TRAINED MODEL')
+    rej_out_path = config['output_path'] + '/trained_input'
     print('Concatenating pairs...')
     rej_pairs = np.concatenate((rej_patches_trn, rej_patches_val, rej_patches_tst), axis=0)
     rej_pairs_ref = np.concatenate((rej_patches_trn_ref, rej_patches_val_ref, rej_patches_tst_ref),axis=0)
