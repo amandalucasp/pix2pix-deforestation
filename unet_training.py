9a84cf93c9b1df520c6059e089bab1bc6c31de9d
Amanda Lucas <amandalucaspereira@gmail.com>
2021-10-07 20:44:12 -0300

    unet trainingadded

--- unet_training.py
+++ unet_training.py
@@ -31,7 +31,7 @@ from skimage.morphology import disk
 from skimage.filters import rank
 import skimage.morphology 
 
-cmap = matplotlib.colors.ListedColormap(['blue', 'olive'])
+from utils import *
 
 stream = open('./config.yaml')
 config = yaml.load(stream, Loader=yaml.CLoader)
@@ -244,7 +244,93 @@ def load_tiles(root_path, testing_tiles_dir, tiles_ts):
   return np.array(img_list), np.array(ref_list)
 
 
+def pred_reconctruct(h, w, num_patches_x, num_patches_y, patch_size_x, patch_size_y, patches_pred):
+    count = 0
+    img_reconstructed = np.zeros((h, w)).astype(np.float32)
+    for i in range(0,num_patches_y):
+        for j in range(0,num_patches_x):
+            img_reconstructed[patch_size_x*j:patch_size_x*(j+1),patch_size_y*i:patch_size_y*(i+1)]=patches_pred[count]
+            count+=1
+    return img_reconstructed
+
+
+def matrics_AA_recall(thresholds_, prob_map, ref_reconstructed, mask_amazon_ts_, px_area):
+    thresholds = thresholds_    
+    metrics_all = []
+    
+    for thr in thresholds:
+        print(thr)  
+
+        img_reconstructed = np.zeros_like(prob_map).astype(np.int8)
+        img_reconstructed[prob_map >= thr] = 1
+    
+        mask_areas_pred = np.ones_like(ref_reconstructed)
+        area = skimage.morphology.area_opening(img_reconstructed, area_threshold = px_area, connectivity=1)
+        area_no_consider = img_reconstructed-area
+        mask_areas_pred[area_no_consider==1] = 0
+        
+        # Mask areas no considered reference
+        mask_borders = np.ones_like(img_reconstructed)
+        #ref_no_consid = np.zeros((ref_reconstructed.shape))
+        mask_borders[ref_reconstructed==2] = 0
+        #mask_borders[ref_reconstructed==-1] = 0
+        
+        mask_no_consider = mask_areas_pred * mask_borders 
+        ref_consider = mask_no_consider * ref_reconstructed
+        pred_consider = mask_no_consider*img_reconstructed
+        
+        ref_final = ref_consider[mask_amazon_ts_==1]
+        pre_final = pred_consider[mask_amazon_ts_==1]
+        
+        # Metrics
+        cm = confusion_matrix(ref_final, pre_final)
+        #TN = cm[0,0]
+        FN = cm[1,0]
+        TP = cm[1,1]
+        FP = cm[0,1]
+        precision_ = TP/(TP+FP)
+        recall_ = TP/(TP+FN)
+        aa = (TP+FP)/len(ref_final)
+        mm = np.hstack((recall_, precision_, aa))
+        metrics_all.append(mm)
+    metrics_ = np.asarray(metrics_all)
+    return metrics_
+    
+def complete_nan_values(metrics):
+    vec_prec = metrics[:,1]
+    for j in reversed(range(len(vec_prec))):
+        if np.isnan(vec_prec[j]):
+            vec_prec[j] = 2*vec_prec[j+1]-vec_prec[j+2]
+            if vec_prec[j] >= 1:
+                vec_prec[j] == 1
+    metrics[:,1] = vec_prec
+    return metrics 
+
+
+image_stack, final_mask = get_dataset(config)
+print('[*] Normalizing image array...')
+image_array = normalization(image_stack.copy(), 3) # -1 to +1
+mask_tiles = create_mask(final_mask.shape[0], final_mask.shape[1], grid_size=(5, 4))
+
+print('[*] Creating padded image...')
+time_ts = []
+n_pool = 3
+n_rows = 5
+n_cols = 4
+rows, cols = image_array.shape[:2]
+pad_rows = rows - np.ceil(rows/(n_rows*2**n_pool))*n_rows*2**n_pool
+pad_cols = cols - np.ceil(cols/(n_cols*2**n_pool))*n_cols*2**n_pool
+npad = ((0, int(abs(pad_rows))), (0, int(abs(pad_cols))), (0, 0))
+image1_pad = np.pad(image_array, pad_width=npad, mode='reflect')
+h, w, c = image1_pad.shape
+patch_size_rows = h//n_rows
+patch_size_cols = w//n_cols
+num_patches_x = int(h/patch_size_rows)
+num_patches_y = int(w/patch_size_cols)
+input_shape=(patch_size_rows,patch_size_cols, c)
+
 # check normalization, maybe apply some map
+print('[*] Loading patches...')
 # Training patches
 patches_train, patches_tr_ref = load_patches(root_path, training_dir)
 # Validation patches
@@ -268,12 +354,13 @@ os.makedirs(output_folder + '/checkpoints', exist_ok=True)
 adam = Adam(lr = 0.0001 , beta_1=0.9)
 # 0: forest, 1: new deforestation, 2: old deforestation
 weights = [0.1, 0.6, 0.0] # desconsidero a classe 2 nesse problema
-print("Weights:", weights)
+print("[*] Weights for CE:", weights)
 loss = weighted_categorical_crossentropy(weights)
 
 cmap = matplotlib.colors.ListedColormap(['blue', 'olive', 'brown'])
 
 for run in range(number_runs):
+
   net = build_unet((patch_size, patch_size, channels), nb_filters, number_class)
   net.summary()
   net.compile(loss = loss, optimizer=adam , metrics=['accuracy'])
@@ -292,7 +379,7 @@ for run in range(number_runs):
   fig.savefig(output_folder + '/accuracy_model_' + str(run) + '.png')
   plt.close(fig)
 
-  # test the model on patches 
+  # test the model on testing patches 
   print(patches_test.shape)
   predicted_labels = test_model(model, patches_test)
   metrics = compute_metrics(patches_test_ref.flatten(), predicted_labels.flatten())
@@ -300,6 +387,212 @@ for run in range(number_runs):
   print("F1 Score:", metrics[1])
   print("Recall:", metrics[2])
   print("Precision:", metrics[3])
+
+  # testing the model
+  new_model = build_unet(input_shape, nb_filters, number_class)
+  # model = load_model(name, compile=False)
+  for l in range(1, len(model.layers)):
+    new_model.layers[l].set_weights(model.layers[l].get_weights())
+  print('Loaded weights for testing model')
+  patch_t = []
+  start_test = time.time()
+  for i in range(0,num_patches_y):
+      for j in range(0,num_patches_x):
+          patch = image1_pad[patch_size_rows*j:patch_size_rows*(j+1), patch_size_cols*i:patch_size_cols*(i+1), :]
+          predictions_ = new_model.predict(np.expand_dims(patch, axis=0)) # talvez era isso que faltava no outro
+          # predicted_class = np.argmax(predictions_, axis=-1)
+          # print(predictions_, predicted_class)
+          del patch 
+          # patch_t.append(predicted_class)
+          patch_t.append(predictions_[:,:,:,1])
+          del predictions_
+  end_test =  time.time() - start_test
+  patches_pred = np.asarray(patch_t).astype(np.float32)
+  print(patches_pred.shape)
+  prob_recontructed = pred_reconctruct(h, w, num_patches_x, num_patches_y, patch_size_rows, patch_size_cols, patches_pred)
+  np.save(output_folder +'/prob_'+str(run)+'.npy',prob_recontructed) 
+  time_ts.append(end_test)
+  del prob_recontructed, model, patches_pred
+
+time_ts_array = np.asarray(time_ts)
+np.save(output_folder + '/metrics_ts.npy', time_ts_array)
+
+prob_rec = np.zeros((image1_pad.shape[0],image1_pad.shape[1], number_runs))
+for run in range (0, number_runs):
+    prob_rec[:,:,run] = np.load(output_folder+'/'+'prob_'+str(run)+'.npy').astype(np.float32)
+
+mean_prob = np.mean(prob_rec, axis = -1)
+np.save(output_folder + '/prob_mean.npy', mean_prob)
+
+# Plot mean map and reference
+fig = plt.figure(figsize=(15,10))
+ax1 = fig.add_subplot(121)
+plt.title('Prediction')
+ax1.imshow(mean_prob, cmap = cmap)
+ax1.axis('off')
+
+ax2 = fig.add_subplot(122)
+plt.title('Reference')
+ax2.imshow(final_mask, cmap = cmap)
+ax2.axis('off')
+fig.savefig('mean_map_and_ref.png')
+
+mean_prob = mean_prob[:final_mask.shape[0], :final_mask.shape[1]]
+
+# print(tiles_ts.shape)
+print(mask_tiles.shape)
+print(mean_prob.shape)
+
+mask_amazon_ts = np.zeros((mask_tiles.shape)).astype('float32')
+for ts_ in tiles_ts:
+    mask_amazon_ts[mask_tiles == ts_] = 1
+
+ref1 = np.ones_like(final_mask).astype(np.float32)
+ref1 [final_mask == 2] = 0
+TileMask = mask_amazon_ts * ref1
+GTTruePositives = final_mask==1
+Pmax = np.max(mean_prob[GTTruePositives * TileMask ==1])
+print(Pmax)
+
+# adicionar funcao de metricas recall e precision
+
+Npoints = 10
+Pmax = np.max(mean_prob[GTTruePositives * TileMask ==1])
+ProbList = np.linspace(Pmax,0,Npoints)
+metrics_ = matrics_AA_recall(ProbList, mean_prob, final_mask, mask_amazon_ts, 625)
+np.save(output_folder+'/acc_metrics.npy',metrics_)
+
+# Complete NaN values
+metrics_copy = metrics_.copy()
+metrics_copy = complete_nan_values(metrics_copy)
+
+# Comput Mean Average Precision (mAP) score 
+Recall = metrics_copy[:,0]
+Precision = metrics_copy[:,1]
+AA = metrics_copy[:,2]
+    
+DeltaR = Recall[1:]-Recall[:-1]
+AP = np.sum(Precision[:-1]*DeltaR)
+print('mAP', AP)
+
+# Plot Recall vs. Precision curve
+plt.close('all')
+fig = plt.figure(figsize=(15,10))
+plt.plot(metrics_copy[:,0],metrics_copy[:,1])
+plt.plot(metrics_copy[:,0],metrics_copy[:,2])
+plt.grid()
+fig.savefig(output_folder + '/roc.png')
+
+# f = open(output_folder + '/results.txt', 'w+')
+# metrics_ = []
+# for tile in tiles_ts:
+#   current_tile_pred = mean_prob[mask_tiles == tile]
+#   print(current_tile_pred.shape)
+#   print(np.unique(current_tile_pred))
+#   tile_mask = final_mask[mask_tiles == tile]
+#   print(tile_mask.shape)
+#   print(np.unique(tile_mask))
+#   metrics = compute_metrics(tile_mask.flatten(), current_tile_pred.flatten())
+#   metrics_.append(metrics)
+#   f.write('metrics - ' + str(tile) + ':\n')
+#   f.write('accuracy, f1score, recall, precision')
+#   f.write(str(metrics[0]) + ',' +  str(metrics[1]) + ',' + str(metrics[2]) + ',' + str(metrics[3]) + '\n')
+
+# # accuracy, f1score, recall, precision
+# final_metrics = np.mean(metrics_)
+# f.write('final_metrics:')
+# f.write('accuracy, f1score, recall, precision')
+# f.write(str(final_metrics[0]) + ',' +  str(final_metrics[1]) + ',' + str(final_metrics[2]) + ',' + str(final_metrics[3]) + '\n')
+
+# f.close()
+
+
+# 000000
+
+# rows, cols = np.where(mask_tiles == 17.)
+# x1 = np.min(rows)
+# y1 = np.min(cols)
+# x2 = np.max(rows)
+# y2 = np.max(cols)
+# print(mean_prob.shape, final_mask.shape)
+# tile_pred = mean_prob[x1:x2 + 1, y1:y2 + 1]
+# tile_ref = final_mask[x1:x2 + 1, y1:y2 + 1]
+# # Plot mean map and reference
+# fig = plt.figure(figsize=(15,10))
+# ax1 = fig.add_subplot(121)
+# plt.title('Prediction')
+# im = ax1.imshow(tile_pred, cmap =cmap)
+# ax1.axis('off')
+# fig.savefig(output_folder + '/prediction_17.png')
+# plt.close()
+# fig = plt.figure(figsize=(15,10))
+# ax2 = fig.add_subplot(122)
+# plt.title('Reference')
+# ax2.imshow(tile_ref, cmap =cmap)
+# # plt.colorbar(im)
+# ax2.axis('off')
+# fig.savefig(output_folder + '/ref_17.png')
+# plt.close()
+
+  # prob_recontructed = pred_reconctruct(h, w, num_patches_x, num_patches_y, patch_size_rows, patch_size_cols, patches_pred)
+  # np.save(output_folder +'/prob_'+str(run)+'.npy',prob_recontructed) 
+  # del prob_recontructed
+  # prob_rec = np.zeros((image1_pad.shape[0],image1_pad.shape[1], times))
+  # prob_rec[:,:,run] = np.load(output_folder + '/prob_'+str(run)+'.npy').astype(np.float32)
+  # prob_rec[prob_rec >= 0.5] = 1
+  # prob_rec[prob_rec < 0.5] = 0
+  # mean_prob = np.mean(prob_rec, axis = -1)
+  # np.save(output_folder + '/prob_mean.npy', mean_prob)
+
+  # rows, cols = np.where(mask_tiles == 17.)
+  # x1 = np.min(rows)
+  # y1 = np.min(cols)
+  # x2 = np.max(rows)
+  # y2 = np.max(cols)
+  # print(mean_prob.shape, final_mask.shape)
+  # tile_pred = mean_prob[x1:x2 + 1, y1:y2 + 1]
+  # tile_ref = final_mask[x1:x2 + 1, y1:y2 + 1]
+  # # Plot mean map and reference
+  # fig = plt.figure(figsize=(15,10))
+  # ax1 = fig.add_subplot(121)
+  # plt.title('Prediction')
+  # im = ax1.imshow(tile_pred, cmap =cmap)
+  # ax1.axis('off')
+  # fig.savefig(output_folder + '/prediction_17.png')
+  # plt.close()
+  # fig = plt.figure(figsize=(15,10))
+  # ax2 = fig.add_subplot(122)
+  # plt.title('Reference')
+  # ax2.imshow(tile_ref, cmap =cmap)
+  # # plt.colorbar(im)
+  # ax2.axis('off')
+  # fig.savefig(output_folder + '/ref_17.png')
+  # plt.close()
+
+  # rows, cols = np.where(mask_tiles == 15.)
+  # x1 = np.min(rows)
+  # y1 = np.min(cols)
+  # x2 = np.max(rows)
+  # y2 = np.max(cols)
+  # print(mean_prob.shape, final_mask.shape)
+  # tile_pred = mean_prob[x1:x2 + 1, y1:y2 + 1]
+  # tile_ref = final_mask[x1:x2 + 1, y1:y2 + 1]
+  # # Plot mean map and reference
+  # fig = plt.figure(figsize=(15,10))
+  # ax1 = fig.add_subplot(121)
+  # plt.title('Prediction')
+  # im = ax1.imshow(tile_pred, cmap =cmap)
+  # ax1.axis('off')
+  # fig.savefig(output_folder + '/prediction_5.png')
+  # plt.close()
+  # fig = plt.figure(figsize=(15,10))
+  # ax2 = fig.add_subplot(122)
+  # plt.title('Reference')
+  # ax2.imshow(tile_ref, cmap =cmap)
+  # # plt.colorbar(im)
+  # ax2.axis('off')
+  # fig.savefig(output_folder + '/ref_5.png')
+  # plt.close()
 
 
   # predicted_tiles = test_model(model, tiles_test)
