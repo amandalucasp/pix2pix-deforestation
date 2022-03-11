5ce880c0f6d9ea9f5de1f958b2541f6088043ccd
Amanda Lucas <mandspereira@gmail.com>
2022-03-11 12:41:00 -0300

    fix mask load

--- utils_unet.py
+++ utils_unet.py
@@ -219,9 +219,8 @@ def to_unet_format(img, mask):
   # [-1, 1] => [0, 2]
   img = img*0.5 + 0.5
   # u-net expects a 1-channel mask
-  # [-1, 0, +1] => [0, 1, 2]
-  mask = mask[:,:,-1]
-  # mask = mask + 1 
+  if mask.ndim > 2:
+    mask = mask[:,:,-1]
   return img, mask
 
 
